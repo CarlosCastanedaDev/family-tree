0f68a4a761849b1b201c4c6d387f1d467090b63e
CarlosCastanedaDev <carlos@nemin.dev>
2023-10-26 15:03:16 -0500

    added associations for siblings

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,4 +9,18 @@ class Person < ApplicationRecord
   has_many :spouses1, through: :marriages1, source: :spouse2
   has_many :marriages2, class_name: "Marriage", foreign_key: "spouse2_id"
   has_many :spouses2, through: :marriages2, source: :spouse1
+
+  # has_many :siblings, through: :parents, source: :children
+
+  # has_many :sibling_relationships, through: :parents, source: :children
+  # has_many :siblings, through: :sibling_relationships, source: :parents, class_name: 'Person'
+
+  # def actual_siblings
+  #   siblings.where.not(id: self.id).distinct
+  # end
+
+  def siblings
+    siblings_ids = parents.flat_map { |parent| parent.children.where.not(id: self.id).pluck(:id) }
+    Person.where(id: siblings_ids)
+  end
 end
