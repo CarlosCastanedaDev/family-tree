16c55a75b4a2fa15a1193059627334bc50d0f5b5
CarlosCastanedaDev <carlos@nemin.dev>
2023-11-27 12:16:02 -0600

    fixed sass error with rails_admin

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.css_compressor = nil
+
 module Daisyui
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
