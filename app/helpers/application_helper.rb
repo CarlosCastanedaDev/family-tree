2c174326fce811edd1e605bb88ab08d301bdf09e
CarlosCastanedaDev <carlos@nemin.dev>
2023-11-28 16:42:05 -0600

    changed form input split from comma to semicolon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def format_ingredients(ingredients)
-    ingredients.split(',').map { |ingredient| content_tag(:li, ingredient.strip, class: "ml-4 pl-2 list-disc") }.join.html_safe
+    ingredients.split(';').map { |ingredient| content_tag(:li, ingredient.strip, class: "ml-4 pl-2 list-disc") }.join.html_safe
   end
 
   def format_directions(directions)
-    directions.split(',').map { |direction| content_tag(:li, direction.strip, class: "ml-4 pl-2 list-decimal") }.join.html_safe
+    directions.split(';').map { |direction| content_tag(:li, direction.strip, class: "ml-4 pl-2 list-decimal") }.join.html_safe
   end
 end
