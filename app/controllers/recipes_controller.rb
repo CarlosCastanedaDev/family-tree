5cb3324cb158c16565fdde769cda461dedd1e08f
CarlosCastanedaDev <carlos@nemin.dev>
2023-11-29 10:53:01 -0600

    added pagination to recipes page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @pagy, @recipes = pagy(Recipe.all.order(created_at: :desc), items: 10)
   end
 
   # GET /recipes/1 or /recipes/1.json
