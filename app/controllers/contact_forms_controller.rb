5dba49779a43dee4e6a482ebf45ab658f59c96d1
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2024-02-01 12:27:53 -0600

    added recaptcha logic to contact form controller

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -2,11 +2,16 @@ class ContactFormsController < ApplicationController
   before_action { authorize(@contact || ContactForm) }
 
   def create
-    name = params[:name]
-    email = params[:email]
-    message = params[:message]
-    ApplicationMailer.contact_form(name, email, message).deliver_now
-    redirect_to root_path, notice: 'Your message was sent successfully.'
+    if verify_recaptcha
+      name = params[:name]
+      email = params[:email]
+      message = params[:message]
+      ApplicationMailer.contact_form(name, email, message).deliver_now
+      redirect_to root_path, notice: 'Your message was sent successfully.'
+    else
+      flash[:alert] = 'Please verify you are not a robot.'
+      render 'layouts/contact_us'
+    end
   end
 
   def contact_us
